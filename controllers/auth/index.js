673a6a79cfac607c5d57c9f6c084120c8c217dee
Devesh Kumar <devesh2027@gmail.com>
2021-08-29 14:40:07 +0530

    Added token generating controller to routes

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import register from "./register";
+import token from "./token";
 
 const authRouter = new Router();
 
 authRouter.post("/register", register);
+authRouter.post("/token", token);
 
 export default authRouter;
