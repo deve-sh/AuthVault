76c02ea88c63b792228d9b064708353e55afa075
Devesh Kumar <devesh2027@gmail.com>
2021-08-29 08:57:21 +0530

    Added auth routers to /api/auth

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+// Controller Routers
+import authRouter from "./controllers/auth";
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
+
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT || 8080, () => console.log("App running."));
