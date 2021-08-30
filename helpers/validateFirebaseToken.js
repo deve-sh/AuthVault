18eab16e501f999ab8b73819fd3cbb62461d9d8e
Devesh Kumar <devesh2027@gmail.com>
2021-08-30 09:43:52 +0530

    Turned validateFirebaseToken into a middleware

--- helpers/validateFirebaseToken.js
+++ helpers/validateFirebaseToken.js
@@ -1,11 +1,16 @@
 import firebaseAdmin from "../firebaseAdmin";
 
-export default async function validateFirebaseToken(idToken) {
+export default async function validateFirebaseToken(req, res, next) {
 	try {
+		let idToken = req.headers["authorization"] || req.headers["Authorization"];
+		if (!idToken) throw new Error("Token missing from request.");
 		let decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
-		return decodedToken;
+		req.token = decodedToken;
+		next();
 	} catch (err) {
 		if (process.env.NODE_ENV !== "production") console.log(err);
-		return null;
+		return res.status(403).json({
+			error: "Unauthorized",
+		});
 	}
 }
