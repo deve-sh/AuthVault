0587a5525720a0965c8d01e845ae69c923609e34
Devesh Kumar <devesh2027@gmail.com>
2021-08-30 20:07:43 +0530

    Integrated listClients controller to clients controllers

--- controllers/clients/index.js
+++ controllers/clients/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import createOAuthClient from "./create";
 import getOAuthClient from "./getClient";
+import listOAuthClient from "./listClients";
 
 const OAuthClientsRouter = new Router();
 
 OAuthClientsRouter.post("/create", createOAuthClient);
-OAuthClientsRouter.post("/get/:clientId", getOAuthClient);
+OAuthClientsRouter.get("/get/:clientId", getOAuthClient);
+OAuthClientsRouter.get("/list", listOAuthClient);
 
 export default OAuthClientsRouter;
