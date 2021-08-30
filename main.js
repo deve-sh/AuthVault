e3ac6c451057cb6a60e67fa717ac4f9d47dbf602
Devesh Kumar <devesh2027@gmail.com>
2021-08-30 09:46:47 +0530

    Integrated OAuthClientsRouter to main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ import helmet from "helmet";
 
 // Controller Routers
 import authRouter from "./controllers/auth";
+import OAuthClientsRouter from "./controllers/clients";
+
+// Middlewares
+import validateFirebaseToken from "./helpers/validateFirebaseToken";
 
 const app = express();
 
@@ -12,5 +16,6 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/clients", validateFirebaseToken, OAuthClientsRouter);
 
 app.listen(process.env.PORT || 8080, () => console.log("App running."));
