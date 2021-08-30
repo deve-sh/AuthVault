1ea931278cf2cece0d623fa2b76d7a4d3d452c6d
Devesh Kumar <devesh2027@gmail.com>
2021-08-30 09:38:23 +0530

    Moved dotenv to root file to avoid missed env variables

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+require("dotenv").config();
 require = require("esm")(module);
 module.exports = require("./main.js");
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import dotenv from "dotenv";
-dotenv.config();
 
 // Controller Routers
 import authRouter from "./controllers/auth";
