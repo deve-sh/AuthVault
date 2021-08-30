107a77e8f964624ac9d44ac1e1b597b2a9cc269a
Devesh Kumar <devesh2027@gmail.com>
2021-08-30 09:41:42 +0530

    Fixed small issue with firebase admin auth in helpers

--- helpers/validateFirebaseToken.js
+++ helpers/validateFirebaseToken.js
@@ -1,6 +1,8 @@
+import firebaseAdmin from "../firebaseAdmin";
+
 export default async function validateFirebaseToken(idToken) {
 	try {
-		let decodedToken = await auth.verifyIdToken(idToken);
+		let decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
 		return decodedToken;
 	} catch (err) {
 		if (process.env.NODE_ENV !== "production") console.log(err);
