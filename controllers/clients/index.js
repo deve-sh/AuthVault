49313420c6ca4b33a3a49b62e13801e52d44de91
Devesh Kumar <devesh2027@gmail.com>
2021-08-31 09:20:02 +0530

    Integrated deleteOAuthClient controller to clients router

--- controllers/clients/index.js
+++ controllers/clients/index.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import createOAuthClient from "./create";
 import getOAuthClient from "./getClient";
 import listOAuthClient from "./listClients";
+import deleteOAuthClient from "./deleteClient";
 
 const OAuthClientsRouter = new Router();
 
 OAuthClientsRouter.post("/create", createOAuthClient);
 OAuthClientsRouter.get("/get/:clientId", getOAuthClient);
 OAuthClientsRouter.get("/list", listOAuthClient);
+OAuthClientsRouter.delete("/delete/:clientId", deleteOAuthClient);
 
 export default OAuthClientsRouter;
