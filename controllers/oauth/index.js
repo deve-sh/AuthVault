fce522486fb22c426e45745d889c9a56f8474e99
Devesh Kumar <devesh2027@gmail.com>
2021-09-01 09:27:12 +0530

    Integrated New OAuth Controllers with OAuth Router

--- controllers/oauth/index.js
+++ controllers/oauth/index.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
 import generateCode from "./code";
+import getOAuthToken from "./getOAuthToken";
+import getUserDetails from "./getUserDetails";
 
 const OAuthRouter = new Router();
 
 OAuthRouter.post("/code", generateCode);
+OAuthRouter.post("/getToken", getOAuthToken);
+OAuthRouter.post("/getUserDetails", getUserDetails);
 
 export default OAuthRouter;
