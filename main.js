37b2dcadd2a0a148d08fbb720981c112d7718460
Devesh Kumar <devesh2027@gmail.com>
2021-09-05 09:24:42 +0530

    Added support for urlencoded values for app

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
 	session({
 		secret: process.env.JWT_SECRET,
